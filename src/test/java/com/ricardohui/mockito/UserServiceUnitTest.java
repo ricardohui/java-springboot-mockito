27c31da05196b7e5d4b7fa7213a945764902c79e
Ricardo Hui <rcdhui@gmail.com>
2021-05-30 16:12:39 +0800

    add argumentCaptor test

--- src/test/java/com/ricardohui/mockito/UserServiceUnitTest.java
+++ src/test/java/com/ricardohui/mockito/UserServiceUnitTest.java
@@ -4,6 +4,9 @@ package com.ricardohui.mockito;
 import static org.assertj.core.api.Assertions.*; // assertion library
 import static org.mockito.Mockito.when; // mock
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,17 +16,32 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 @SpringBootTest()
 public class UserServiceUnitTest {
     @Autowired
-    private UserService userService;
+    private UserService systemUnderTest;
 
     @MockBean
     private NameService nameService;
 
+    @Captor
+    ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
 
 
     @Test
     public void whenUserIdIsProvided_thenRetrievedNameIsCorrect(){
         when(nameService.getUserName("SomeId")).thenReturn("Mock user name");
-        String testName = userService.getUserName("SomeId");
+        String testName = systemUnderTest.getUserName("SomeId");
         assertThat(testName).isEqualTo("Mock user name");
+
     }
+
+    @Test
+    public void whenUserIdIsProvided_verifyItAlwaysReturnLowercaseId(){
+        systemUnderTest.getUserName("SomeId");
+        Mockito.verify(nameService).getUserName(captor.capture());
+        String actual = captor.getValue();
+        assertThat(actual).isEqualTo("SomeId");
+    }
+
+
+
+
 }
